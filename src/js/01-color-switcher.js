04cb8bb1f48da139c8e3efb281384e4fc8ba07c0
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-01-31 11:56:59 +0200

    Сделал 1 задачу

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const TIME_INTERVAL = 1000;
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.body,
+};
+
+refs.startBtn.addEventListener('click', onStartBtn);
+refs.stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+  if (!refs.startBtn.disabled) {
+    refs.startBtn.disabled = true;
+    timerId = setInterval(() => {
+      refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, TIME_INTERVAL);
+  }
+}
+
+function onStopBtn() {
+  refs.startBtn.disabled = false;
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+}
+
+function getRandomHexColor() {
+  console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
